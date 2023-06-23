a35e9048e7c7c79c43e5c0613161e6924a83a9a5
maulanadiki <maulanadiki23@gmail.com>
2023-06-23 14:22:36 +0700

    penambahan script css di skill

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import './service.css'
 const Service = () => {
+    const [toggle,setToggle] = useState(0)
+
+    const toggleTab = (index)=>{
+        setToggle(index);
+    }
   return (
     <section className="services section" id="services">
         <h2 className="section__title">Services</h2>
@@ -13,19 +19,19 @@ const Service = () => {
                     <h3 className="services__title">Product <br /> Designer</h3>
                 </div>
 
-                <span className="services__button">
+                <span className="services__button" onClick={()=>toggleTab(1)}>
                     view more <i className="uil uil-arrow-right services__button-icon"></i>
                 </span>
 
-                <div className="services__modal">
+                <div className={toggle === 1 ? "services__modal active-modal" : "services__modal"}>
                     <div className="services__modal-content">
-                        <i className="uil uil-times services__modal-close"></i>
+                        <i className="uil uil-times services__modal-close" onClick={()=>toggleTab(0)}></i>
                         <h3 className="services__modal-title">Product Designer</h3>
                         <p className="services__modal-description">
                             Service with more than 3 years of experience. providing quality work to clients and companies
                         </p>
 
-                        <ul className="services__modal-service">
+                        <ul className="services__modal-services">
                             <li className="services__modal-service">
                                 <i className="uil uil-check-circle services__modal-icon"></i>
                                 <p className="services__modal-info">i develop the user interface</p>
@@ -63,13 +69,13 @@ const Service = () => {
                     </h3>
                 </div>
 
-                <span className="services__button">
+                <span className="services__button"  onClick={()=>toggleTab(2)}>
                     view more <i className="uil uil-arrow-right services__button-icon"></i>
                 </span>
 
-                <div className="services__modal">
+                <div className={toggle === 2 ? "services__modal active-modal" : "services__modal"}>
                     <div className="services__modal-content">
-                        <i className="uil uil-times services__modal-close"></i>
+                        <i className="uil uil-times services__modal-close" onClick={()=>toggleTab(0)}></i>
                         <h3 className="services__modal-title">UI /UX Designer</h3>
                         <p className="services__modal-description">
                             Service with more than 3 years of experience. providing quality work to clients and companies
@@ -113,13 +119,13 @@ const Service = () => {
                     </h3>
                 </div>
 
-                <span className="services__button">
+                <span className="services__button" onClick={()=>toggleTab(3)}>
                     View More<i className="uil uil-arrow-right services__button-icon"></i>
                 </span>
 
-                <div className="services__modal">
+                <div className={toggle === 3 ? "services__modal active-modal" : "services__modal"}>
                     <div className="services__modal-content">
-                        <i className="uil uil-times services__modal-close"></i>
+                        <i className="uil uil-times services__modal-close"  onClick={()=>toggleTab(0)}></i>
                         <h3 className="services__modal-title"> Visual Designer</h3>
                         <p className="services__modal-description">
                             Service with more than 3 years of experience. providing quality work to clients and companies
